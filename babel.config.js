cb040fc14c023b14ba53fbd84e6d393f77979d4c
Caio Furtado <caiohbfurtado@gmail.com>
2022-01-07 10:32:04 -0300

    Solving the React Native Challenge: OAuth using Twitch API Credentials

--- babel.config.js
+++ babel.config.js
@@ -3,8 +3,8 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      'inline-dotenv',
-      'react-native-reanimated/plugin'
+      'react-native-reanimated/plugin',
+      ['inline-dotenv', { path: '.env'}],
     ]
   };
 };
